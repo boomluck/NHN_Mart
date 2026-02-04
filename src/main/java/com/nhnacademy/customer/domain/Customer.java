d35b8055bdc6134034717ffc7cb46660be88f963
{boomluck} <{boomluck@gmail.com}>
2026-02-04 17:35:48 +0900

    코드 작성 완료.
    equals()와 hashcode() 이해하는 데에 오후를 다 소비함.

--- src/main/java/com/nhnacademy/customer/domain/Customer.java
+++ src/main/java/com/nhnacademy/customer/domain/Customer.java
@@ -28,50 +28,71 @@ public class Customer {
 
     public Customer(long id, String name, int money) {
         // TODO#1-1 id < 1 또는 name이 null이거나 ""인 경우, 또는 money < 0이면 IllegalArgumentException이 발생합니다.
+        if (id < 1 || name == null || name.isBlank() || money < 0) {
+            throw new IllegalArgumentException("오류 : 아이디, 이름, 잔액 중 하나가 잘못되었습니다.");
+        }
 
         // TODO#1-2 id, name, money를 초기화합니다.
+        this.id = id;
+        this.name = name;
+        this.money = money;
     }
 
     public long getId() {
         // TODO#1-3 메서드를 구현하세요. id를 반환합니다.
-        return 0L;
+        return id;
     }
 
     public String getName() {
         // TODO#1-4 메서드를 구현하세요. name을 반환합니다.
-        return null;
+        return name;
     }
 
     public int getMoney() {
         // TODO#1-5 메서드를 구현하세요. money를 반환합니다.
-        return 0;
+        return money;
     }
 
     public void pay(int amount) throws InsufficientFundsException {
         // TODO#1-6 amount(결제할 금액) < 0이면 IllegalArgumentException이 발생합니다.
+        if (amount < 0) {
+            throw new IllegalArgumentException("결제할 금액이 없거나 잘못되었습니다.");
+        }
 
         // TODO#1-7 money(회원 보유 금액) < amount(결제할 금액)이면 InsufficientFundsException이 발생합니다.
+        if (money < amount) {
+            throw new InsufficientFundsException();
+        }
 
         // TODO#1-8 메서드를 구현합니다. money에서 amount만큼 차감합니다.
-
+        money -= amount;
         log.debug("customer: {}, pay : {}", this, amount);
     }
 
     @Override
     public String toString() {
         // TODO#1-9 id, name, money가 반환될 수 있도록 구현합니다.
-        return null;
+        return "ID : " + id + "NAME : " + name + "MONEY : " + money;
     }
 
     // TODO#1-10 Customer 객체 비교를 위해 구현합니다. (비교 기준: id, name, money 일치)
     @Override
     public boolean equals(Object o) {
-        return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Customer c = (Customer) o;
+
+        return id == c.id && money == c.money && Objects.equals(name, c.name);
     }
 
     // TODO#1-11 (id, name, money) 기준으로 hashCode()를 구현합니다.
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(id, name, money);
     }
 }
--- src/test/java/com/nhnacademy/customer/domain/CustomerTest.java
+++ src/test/java/com/nhnacademy/customer/domain/CustomerTest.java
@@ -33,6 +33,8 @@ class CustomerTest {
     @DisplayName("id < 0")
     void testConstructor1(){
         // TODO#1-12 id < 1 이면 IllegalArgumentException이 발생하는지 검증합니다.
+
+        assertThrows(IllegalArgumentException.class, () -> new Customer(0L, "NHN아카데미",100_0000));
     }
 
     @Order(2)
@@ -40,6 +42,7 @@ class CustomerTest {
     @DisplayName("money < 0")
     void testConstructor3() {
         // TODO#1-13 Customer 생성 시 money < 0이면 IllegalArgumentException이 발생하는지 검증합니다.
+        assertThrows(IllegalArgumentException.class, () -> new Customer(1L, "NHN아카데미",0));
     }
 
     @Order(3)
@@ -47,24 +50,29 @@ class CustomerTest {
     @DisplayName("name is ( empty or null ) ")
     void testConstructor2(){
         // TODO#1-14 name이 "" 또는 null이면 IllegalArgumentException.class 예외가 발생하는지 검증합니다.
+        assertThrows(IllegalArgumentException.class, () -> new Customer(0L, null,100_0000));
+        assertThrows(IllegalArgumentException.class, () -> new Customer(0L, "",100_0000));
     }
 
     @Order(4)
     @Test
     void getId() {
         // TODO#1-15 customer -> getId() 호출 시 1L을 반환하는지 검증합니다.
+        assertEquals(1L, customer.getId());
     }
 
     @Order(5)
     @Test
     void getName() {
         // TODO#1-16 customer -> getName() 호출 시 "NHN아카데미"를 반환하는지 검증합니다.
+        assertEquals("NHN아카데미", customer.getName());
     }
 
     @Order(6)
     @Test
     void getMoney() {
         // TODO#1-17 customer -> getMoney() 호출 시 100_0000을 반환하는지 검증합니다.
+        assertEquals(100_0000, customer.getMoney());
     }
 
     @Order(7)
@@ -72,6 +80,8 @@ class CustomerTest {
     @DisplayName("결제 : 100_0000 - 10_0000 = 90_0000")
     void pay1() throws InsufficientFundsException {
         // TODO#1-18 10_0000 결제 후 보유 금액이 90_0000이 되는지 검증합니다.
+        customer.pay(100000);
+        assertEquals(900000, customer.getMoney());
     }
 
     @Order(8)
@@ -79,6 +89,7 @@ class CustomerTest {
     @DisplayName("결제 amount < 0 ")
     void pay2() throws InsufficientFundsException {
         // TODO#1-19 결제 금액이 0보다 작으면 IllegalArgumentException이 발생하는지 검증합니다.
+        assertThrows(IllegalArgumentException.class, () -> customer.pay(-10000));
     }
 
     @Order(9)
@@ -86,6 +97,7 @@ class CustomerTest {
     @DisplayName("customer money = 100만원, 200만원 결제 시도")
     void pay3(){
         // TODO#1-20 200만 원 결제 시 InsufficientFundsException.class 예외가 발생하는지 검증합니다.
+        assertThrows(InsufficientFundsException.class, () -> customer.pay(2000000));
     }
 
     @Order(10)
@@ -93,6 +105,8 @@ class CustomerTest {
     @DisplayName("id와 name, money가 일치하면 동일한 객체로 식별")
     void testEquals1() {
         // TODO#1-21 id, name, money가 일치하면 equals가 true를 반환하는지 검증합니다.
+        Customer c2 = new Customer(1L, "NHN아카데미",100_0000);
+        assertEquals(true, customer.equals(c2));
     }
 
     @Order(11)
@@ -100,6 +114,8 @@ class CustomerTest {
     @DisplayName("name, money 일치, 아이디는 불일치")
     void testEquals2() {
         // TODO#1-22 id가 다르면 equals가 false를 반환하는지 검증합니다.
+        Customer c2 = new Customer(2L, "NHN아카데미",100_0000);
+        assertEquals(false, customer.equals(c2));
     }
 
 }
